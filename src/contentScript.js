0255cacbaf0e3a374714147904e96db0ca45b719
Eason Lin <uu9924079@gmail.com>
2025-05-08 21:28:33 +0800

    fix: replace textContent one by one

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,8 +2,14 @@ const zh_cn = require('./terms/zh_CN');
 const zh_tw = require('./terms/zh_TW');
 const cn2tw = require('./utils/cn2tw');
 
-let body = document.body.innerHTML;
-
-const newBody = cn2tw({ body, zh_cn, zh_tw });
-
-document.body.innerHTML = newBody;
+function replaceTextNodes(node, replaceFn) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    const res = replaceFn(node.textContent);
+    node.textContent = res;
+  } else if (node.nodeType === Node.ELEMENT_NODE && node.nodeName !== "SCRIPT" && node.nodeName !== "STYLE") {
+    for (let child of node.childNodes) {
+      replaceTextNodes(child, replaceFn);
+    }
+  }
+}
+replaceTextNodes(document.body, text => cn2tw({ body: text, zh_cn, zh_tw }));
